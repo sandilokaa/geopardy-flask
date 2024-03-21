cb89a6b2874bab9c95028de092f82fced433a065
sandilokaa <sandyloka790@gmail.com>
2024-03-22 01:16:35 +0700

    docs: update endpoint predict

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 import pickle
 import pandas as pd
+from sklearn.metrics import accuracy_score
 
 app = Flask(__name__)
 CORS(app)
+
+#---- Import the Utils ----#
 
 with open('./utils/accuracy.pkl', 'rb') as file:
     accuracy_ensemble = pickle.load(file)
@@ -14,6 +17,11 @@ with open('./utils/logreg_model.pkl', 'rb') as file:
 
 with open('./utils/gb_model.pkl', 'rb') as file:
     gb_model = pickle.load(file)
+
+#---- End Import the Utils ----#
+    
+
+#---- Define Endpoint ----#
 
 @app.route('/accuracy', methods=['GET'])
 def rainfall_accuracy():
@@ -26,37 +34,24 @@ def rainfall_accuracy():
     })
 
 
-@app.route('/predict', methods=['GET'])
+@app.route('/predict', methods=['POST'])
 def rainfall_predict():
 
-    new_data = pd.DataFrame({
-        'MinTemp': 20.2,
-        'MaxTemp': 32.3,
-        'Rainfall': 5.2, 
-        'Evaporation': 12.3,
-        'Sunshine': 12.8, 
-        'WindGustSpeed': 56.3, 
-        'WindSpeed9am': 45.3, 
-        'WindSpeed3pm': 32.4, 
-        'Humidity9am': 43.6, 
-        'Humidity3pm': 23.5, 
-        'Pressure9am': 1700.43, 
-        'Pressure3pm': 1023.4, 
-        'Cloud9am': 72.3, 
-        'Cloud3pm': 51.3, 
-        'Temp3pm': 42.2,
-        'RainToday': 1
-    }, [0])
+    data = request.json
+    new_data = pd.DataFrame(data, index=[0])
 
     ensemble_prob_new = (logreg_model.predict_proba(new_data)[:, 1] + gb_model.predict_proba(new_data)[:, 1]) / 2
     ensemble_pred_new = (ensemble_prob_new > 0.5).astype(int)
 
     prediction_result = {
         'Probability': float(ensemble_prob_new),
-        'Prediction': 'Yes, it will rain.' if ensemble_pred_new == 1 else 'No, it will not rain.'
+        'Prediction': 'Yes, it will rain.' if ensemble_pred_new > 0.5 else 'No, it will not rain.'
     }
 
     return jsonify(prediction_result)
+
+
+#---- Define Endpoint ----#
 
 if __name__ == "__main__":
     app.run(debug=True)
